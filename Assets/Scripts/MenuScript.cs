3e3f9296c2ca7468791c2a632a2acf596bc6ce62
Mari Rann <ms.rann333@gmail.com>
2015-04-12 17:23:15 -0700

    - Added main menu scene with [Play] and [Quit] buttons - Clicking [Play] starts the game - Clicking [Quit] brings up a confirmation window

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button playButton;
+	public Button quitButton;
+
+	// Use this for initialization
+	void Start () {
+	
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		playButton = playButton.GetComponent<Button> ();
+		quitButton = quitButton.GetComponent<Button> ();
+		quitMenu.enabled = false;
+	}
+	
+	public void QuitPressed()
+	{
+		quitMenu.enabled = true;
+		playButton.enabled = false;
+		quitButton.enabled = false;
+	}
+
+	public void NoPressed()
+	{
+		quitMenu.enabled = false;
+		playButton.enabled = true;
+		quitButton.enabled = true;
+	}
+
+	public void StartGame()
+	{
+		Application.LoadLevel ("Main Scene");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+}
