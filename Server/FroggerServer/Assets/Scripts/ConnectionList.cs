e32602092a256cffa1f8ef0fb03b863ce1240c24
Hector <hectoram@uci.edu>
2015-04-25 14:22:01 -0700

    Ground work for server side.

---
+++ Server/FroggerServer/Assets/Scripts/ConnectionList.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectionList : MonoBehaviour {
+
+	//Define some container for connections
+	//Find out how to link scripts. 
+	//GetComponent maybe
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
---
+++ Server/FroggerServer/Assets/Scripts/Player.js
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	//Define player as a unique ID
+	//Give them a "link" to their network info.
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
