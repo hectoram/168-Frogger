aa55402256104eef6cf2f2a548daaca9943f1b55
Hector Martinez <hectoram@uci.edu>
2015-05-02 15:39:02 -0700

    Circle of life for the server. Empty classes only.

---
+++ Server/FroggerServer/FroggerServer/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FroggerServer
+{
+    class Player
+    {
+        private string username;
+
+        Player(string name) 
+        {
+            username = name;
+        }
+
+    }
+}
