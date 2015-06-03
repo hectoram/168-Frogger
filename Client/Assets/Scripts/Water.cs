3ceeee8ebd649f63e47f01b19e03cca3b2b25ee3
Mari Rann <ms.rann333@gmail.com>
2015-06-02 21:39:19 -0700

    - Fixed bugs with dying to water

--- Client/Assets/Scripts/Water.cs
+++ Client/Assets/Scripts/Water.cs
@@ -17,17 +17,17 @@ public class Water : MonoBehaviour {
 	void OnTriggerStay2D(Collider2D coll)
 	{
 		// Check to see if the object is a frog
-        if (coll.name == "none")
+        if (coll.tag == "Player")
 			// Check to see if the frog isn't jumping
-		if (!coll.GetComponent<Frog> ().isJumping ())
+		    if (!coll.GetComponent<Frog> ().isJumping ())
 				// Check to see if the frog isn't on a platform
-		if (coll.transform.parent == null && coll.transform.position.y > -1 && coll.transform.position.y < 5) {
-			// GAME OVER
-			//Destroy (coll.gameObject);
-			//menu.ShowLoseMenu();
-            Debug.Log("WATER DEATHHH!");
-            gameUI.addDeathScore();
-            coll.gameObject.transform.position = new Vector3(0, -8, 0);
+		        if (coll.transform.parent == null && coll.transform.position.y > -1 && coll.transform.position.y < 5) {
+			        // GAME OVER
+			        //Destroy (coll.gameObject);
+			        //menu.ShowLoseMenu();
+                    Debug.Log("WATER DEATHHH!");
+                    gameUI.addDeathScore();
+                    coll.gameObject.transform.position = new Vector3(0, -8, 0);
 		}
 	}
 }
